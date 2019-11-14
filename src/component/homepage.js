2026acf411907b6ac4aef0f96a5a44f502e0795d
addo <forison6205@gmail.com>
2019-11-14 05:35:58 -0500

    added starter configs

---
+++ src/component/homepage.js
@@ -0,0 +1,5 @@
+const homepage = () => {
+	const element = document.createElement('div');
+	return element
+}
+export { homepage as default };
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+import homepage from './component/homepage';
+
+homepage();
+
+document.body.appendChild(homepage());
